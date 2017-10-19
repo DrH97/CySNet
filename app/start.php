21c3a92d561805d652b0e6e37120d2a40bffa3fc
drh <jmnabangi@gmail.com>
2017-10-19 19:18:29 +0300

    Initial Commit
    Setting the variables and all

---
+++ app/start.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+define('APP_ROOT', __DIR__);
+define('VIEW_ROOT', APP_ROOT . '/views');
+define('BASE_URL', 'http://localhost/tech-e');
+define('IMAGES', BASE_URL . '/images' . '/');
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=tech-e', 'root', '');
+$db1 = mysqli_connect('localhost','root','');
+    mysqli_select_db($db1, 'tech-e');
+require 'functions.php';
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'app/start.php';
+
+$HardwareProducts = $db->query("
+	SELECT *
+	FROM products
+")->fetchAll(PDO::FETCH_ASSOC);
+
+$Categories = $db->query("
+  SELECT *
+  FROM categories
+")->fetchAll(PDO::FETCH_ASSOC);
+
+$Repairers = $db->query("
+	SELECT *
+	FROM products
+")->fetchAll(PDO::FETCH_ASSOC);
+
+//require VIEW_ROOT . '/templates/pageloader.php';
+
+require VIEW_ROOT . '/home.php';
