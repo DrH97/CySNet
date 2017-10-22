4d2d658fe824f96cff56cd92e16e6cb01bdc689c
drh <jmnabangi@gmail.com>
2017-10-22 20:36:35 +0300

    Added fusion chart and admin login

--- admin/admin.php
+++ admin/admin.php
@@ -1,29 +1,82 @@
 <?php
-    include_once '../app/start.php';
+include_once '../app/start.php';
+if (null !== ($_SESSION['admin'][0])) { ?>
+
+    <script src="<?php echo '../assets/js/fusioncharts.js'; ?>"></script>
+    <script src="<?php echo '../assets/js/fusioncharts.charts.js'; ?>"></script>
+    <script src="<?php echo '../assets/js/fusioncharts.theme.ocean.js'; ?>"></script>
+    <script src="<?php echo '../assets/js/fusioncharts.theme.fint.js'; ?>"></script>
+    <script src="<?php echo '../assets/js/fusioncharts.theme.carbon.js'; ?>"></script>
+
+
+    <?php
+
     require_once VIEW_ROOT . 'layouts/appheader.php';
+    include("fusioncharts.php");
+
+    $totalUsers = 0;
 
     $adminq = $db->query("SELECT * FROM admins");
 
     $admindeets = $adminq->fetch_assoc();
 
-?>
+    $users = $db->query("SELECT EXTRACT(YEAR FROM created_at) AS year, EXTRACT(MONTH FROM created_at) AS month, COUNT(id) AS registeredUsers FROM users GROUP BY MONTH(created_at)");
+
+    foreach ($users as $userrs)
+        $totalUsers += $userrs['registeredUsers'];
+
+    $arrayData = array(
+        "chart" => array(
+            "caption" => "Number of users Registered Per Month",
+            "subaCaption" => "Months",
+            "captionFontSize" => "24",
+            "refreshinterval" => "5",
+            "xAxisName" => "Month",
+            "yAxisName" => "Users",
+            "paletteColors" => "#A2A5FC, #41CBE3, #EEDA54, #BB423F, #F35685",
+            "showValues" => "1",
+            "theme" => 'carbon',
+            "canvasBgColor" => '#FFFFFF,#FFFFFF',
+            "baseFont" => "Roboto",
+        )
+    );
+
+    $arrayData["data"] = array();
+
+    while ($usersRow = mysqli_fetch_array($users)) {
+        array_push($arrayData["data"], array(
+                "label" => $usersRow['month'] . ' ' . $usersRow['year'],
+                "value" => $usersRow["registeredUsers"]
+            )
+        );
+    }
+
+    $encodedData = json_encode($arrayData);
+
+    //var_dump($encodedData);
+
+    $chart = new FusionCharts("doughnut3D", "Users", 600, 400, "user-chart", "json", "$encodedData");
+    $chart->render();
+
+
+    ?>
 
     <style>
         header {
             background: rgba(255, 255, 255, 0.8);
         }
-        
-        table{
+
+        table {
             padding-top: 0em;
             width: 100%;
             height: auto;
             text-align: center;
         }
-        
+
     </style>
 
 
-<body>
+    <body>
 
     <!-- TOP NAVIGATION BAR -->
     <header>
@@ -31,258 +84,27 @@
         <a href="#" id="title">TechCrowd <span id="subtitle">ADMIN</span></a>
         <div id="links">
             <a href="#" style="color: black;background-color: skyblue;">Verify</a>
-            <a href="#" id="logout">Log out</a>
+            <a href="<?php echo BASE_URL . 'auth/auth.php?logout=true' ?>" id="logout">Log out</a>
         </div>
         <div class="account">
             <p><?php echo $admindeets['firstname'] . ' ' . $admindeets['lastname']; ?></p>
-            <img src="<?php echo ASSETS . 'images/favicon.ico'; ?>" />
+            <img src="<?php echo ASSETS . 'images/favicon.ico'; ?>"/>
         </div>
 
     </header>
 
     <!-- PROFILE BOX -->
     <div class="profile-box" style="width: 0;">
-<!--        <center>-->
-<!--            <div>-->
-<!--<!--                <img src="{!! asset('images/defaultprofile.png') !!}" />-->
-<!--<!--                <h3>John Doe</h3>-->
-<!--            </div>-->
-<!--        </center>-->
+        <!--        <center>-->
+        <!--            <div>-->
+        <!--<!--                <img src="{!! asset('images/defaultprofile.png') !!}" />-->
+        <!--<!--                <h3>John Doe</h3>-->
+        <!--            </div>-->
+        <!--        </center>-->
     </div>
 
     <!-- CONTENT -->
     <div class="content">
-
-        <!--chart dashboard start-->
-        <div id="chart-dashboard">
-            <div class="row">
-                <div class="col s12 m8 l8">
-                    <div class="card">
-                        <div class="card-move-up waves-effect waves-block waves-light">
-                            <div class="move-up cyan darken-1">
-                                <div>
-                                    <span class="chart-title white-text">Revenue</span>
-                                    <div class="chart-revenue cyan darken-2 white-text">
-                                        <p class="chart-revenue-total">$500.85</p>
-                                        <p class="chart-revenue-per"><i class="mdi-navigation-arrow-drop-up"></i> 21.80 %</p>
-                                    </div>
-                                    <div class="switch chart-revenue-switch right">
-                                        <label class="cyan-text text-lighten-5">
-                                            Month
-                                            <input type="checkbox">
-                                            <span class="lever"></span> Year
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="trending-line-chart-wrapper">
-                                    <canvas id="trending-line-chart" height="70"></canvas>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-content">
-                            <a class="btn-floating btn-move-up waves-effect waves-light darken-2 right"><i class="mdi-content-add activator"></i></a>
-                            <div class="col s12 m3 l3">
-                                <div id="doughnut-chart-wrapper">
-                                    <canvas id="doughnut-chart" height="200"></canvas>
-                                    <div class="doughnut-chart-status">4500
-                                        <p class="ultra-small center-align">Sold</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col s12 m2 l2">
-                                <ul class="doughnut-chart-legend">
-                                    <li class="Repairers ultra-small"><span class="legend-color"></span>Repairers</li>
-                                    <li class="Customers ultra-small"><span class="legend-color"></span>Customers</li>
-                                    <li class="Sellers ultra-small"><span class="legend-color"></span>Sellers</li>
-                                </ul>
-                            </div>
-                            <div class="col s12 m5 l6">
-                                <div class="trending-bar-chart-wrapper">
-                                    <canvas id="trending-bar-chart" height="90"></canvas>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card-reveal">
-                            <span class="card-title grey-text text-darken-4">Revenue by Month <i class="mdi-navigation-close right"></i></span>
-                            <table class="responsive-table">
-                                <thead>
-                                <tr>
-                                    <th data-field="id">ID</th>
-                                    <th data-field="month">Month</th>
-                                    <th data-field="item-sold">Item Sold</th>
-                                    <th data-field="item-price">Item Price</th>
-                                    <th data-field="total-profit">Total Profit</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>January</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>February</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>March</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>April</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>5</td>
-                                    <td>May</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>6</td>
-                                    <td>June</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>7</td>
-                                    <td>July</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>8</td>
-                                    <td>August</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>9</td>
-                                    <td>Septmber</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>10</td>
-                                    <td>Octomber</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>11</td>
-                                    <td>November</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                <tr>
-                                    <td>12</td>
-                                    <td>December</td>
-                                    <td>122</td>
-                                    <td>100</td>
-                                    <td>$122,00.00</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-
-
-                    </div>
-                </div>
-
-                <div class="col s12 m4 l4">
-                    <div class="card">
-                        <div class="card-move-up teal waves-effect waves-block waves-light">
-                            <div class="move-up">
-                                <p class="margin white-text">Browser Stats</p>
-                                <canvas id="trending-radar-chart" height="114"></canvas>
-                            </div>
-                        </div>
-                        <div class="card-content  teal darken-2">
-                            <a class="btn-floating btn-move-up waves-effect waves-light darken-2 right"><i class="mdi-content-add activator"></i></a>
-                            <div class="line-chart-wrapper">
-                                <p class="margin white-text">Revenue by product</p>
-                                <canvas id="line-chart" height="114"></canvas>
-                            </div>
-                        </div>
-                        <div class="card-reveal">
-                            <span class="card-title grey-text text-darken-4">Revenue by product <i class="mdi-navigation-close right"></i></span>
-                            <table class="responsive-table">
-                                <thead>
-                                <tr>
-                                    <th data-field="country-name">Country Name</th>
-                                    <th data-field="item-sold">Item Sold</th>
-                                    <th data-field="total-profit">Total Profit</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>USA</td>
-                                    <td>65</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>UK</td>
-                                    <td>76</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>Canada</td>
-                                    <td>65</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>Brazil</td>
-                                    <td>76</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-
-                                    <td>India</td>
-                                    <td>65</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>France</td>
-                                    <td>76</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>Austrelia</td>
-                                    <td>65</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                <tr>
-                                    <td>Russia</td>
-                                    <td>76</td>
-                                    <td>$452.55</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--chart dashboard end-->
 
         <div class="tab">
             <button class="tablinks" onclick="openTabs(event, 'dashboard')" id="defaultOpen">DASHBOARD</button>
@@ -303,7 +125,7 @@
                     <th>Inactive repairers</th>
                 </tr>
                 <tr>
-                    <td>123</td>
+                    <td><?php echo $totalUsers; ?></td>
                     <td>43</td>
                     <td>421</td>
                     <td>378</td>
@@ -321,7 +143,7 @@
 
             <center>
                 <div>
-                    <img src="{{ asset('images/defaultprofile.png') }}" />
+                    <img src="<?php echo ASSETS . 'images/defaultprofile.png'; ?>"/>
                     <p>John a guy with long-ass name</p>
                     <button id="view">View Profile</button>
                     <button id="accept">Accept</button>
@@ -335,7 +157,7 @@
 
             <center>
                 <div>
-                    <img src="assets/images/default-profile.png" />
+                    <img src="<?php echo ASSETS . 'images/defaultprofile.png'; ?>"/>
                     <p>John Doe</p>
                     <button id="view">View Profile</button>
                     <button id="accept">Accept</button>
@@ -345,9 +167,7 @@
 
         </div>
 
-
-
-
+        <div id="user-chart"></div>
 
 
     </div>
@@ -379,13 +199,11 @@
         document.getElementById("defaultOpen").click();
     </script>
 
-    <!-- chartist -->
-    <script type="text/javascript" src="<?php echo ASSETS . 'js/chartist-js/chartist.min.js'; ?>"></script>
+    </body>
 
-    <!-- chartjs -->
-    <script type="text/javascript" src="<?php echo ASSETS . 'js/chartjs/chart.min.js' ?>"></script>
-    <script type="text/javascript" src="<?php echo ASSETS . 'js/chartjs/chart-script.js' ?>"></script>
+    <?php include_once VIEW_ROOT . 'layouts/footer.php';
 
-</body>
-
-<?php include_once VIEW_ROOT . 'layouts/footer.php'; ?>
+} else {
+    header("LOCATION: " . BASE_URL . 'auth/adminlogin.php');
+}
+?>
---
+++ auth/adminlogin.php
@@ -0,0 +1,187 @@
+<?php
+require '../app/start.php';
+require VIEW_ROOT . 'layouts/appheader.php'; ?>
+
+
+<style>
+        
+        .body{
+            background-color: skyblue;
+        }
+        
+        .content h2 {
+            margin-top: 2em;
+            text-align: center;
+        }
+        
+        div.tab {
+            overflow: hidden;
+            border: 1px solid #ccc;
+            background-color: transparent;
+            padding: 0 32.6% 0 32.6%;
+            margin-top: 2em;
+            border-style: none;
+        }
+        
+        .tabcontent {
+            border-style: none;
+        }
+        /* Style the buttons inside the tab */
+        
+        div.tab button {
+            background-color: inherit;
+            float: left;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 14px 16px;
+            transition: 0.3s;
+            color: black;
+            margin: 0 1em;
+            border: 1px solid white;
+        }
+        /* Change background color of buttons on hover */
+        
+        div.tab button:hover {
+            color: dodgerblue;
+            background-color: white;
+            border: 1px solid dodgerblue;
+        }
+        /* Create an active/current tablink class */
+        
+        div.tab button.active {
+            color: white;
+            background-color: dodgerblue;
+            border-style: none;
+        }
+
+        form a{
+            color: dodgerblue;
+            font-size: 13px;
+            text-decoration: none;
+            position: relative;
+            top: 10px;
+        }
+    </style>
+
+    <!-- TOP NAVIGATION BAR -->
+    <header>
+
+        <center>
+            <a href="<?php echo BASE_URL; ?>" id="title">TechCrowd<span id="subtitle"> WELCOME</span></a>
+        </center>
+
+    </header>
+
+<div class="body" style="height: 93vh; overflow: scroll;">
+    <!-- CONTENT -->
+    <div class="content">
+
+        <h2>Hey there, welcome to <span style="font-size: 1.2em;"><b>TechCrowd</b></span> :)</h2>
+
+        <!-- TABS -->
+        <div class="tab">
+            <button class="tablinks" onclick="openTabs(event, 'login')" id="defaultOpen">LOGIN</button>
+            <button class="tablinks" onclick="openTabs(event, 'signup')">CREATE NEW ACCOUNT</button>
+        </div>
+
+        <!-- LOGIN -->
+        <div id="login" class="tabcontent">
+
+            <center>
+
+            <form method="post" action="auth.php?login=true">
+
+                <input id="email" type="email" placeholder="Email" name="email" value="" required autofocus>
+                <br>
+
+                <input id="password" type="password" placeholder="Password" name="password" required><br><br>
+
+                <label>
+                    <input type="checkbox" name="remember">
+                    Remember Me
+                </label><br><br>
+
+                <input type="submit" value="LOG IN" name="login"><br>
+
+                <a href="">
+                    Forgot Your Password?
+                </a>
+
+            </form>
+            </center>
+
+        </div>
+
+
+        <!-- SIGNUP -->
+        <div id="signup" class="tabcontent">
+
+            <center>
+
+            <form method="post" action="auth.php?register=true">
+
+                <div display="inline-flex">
+                    <input id="firstname" type="text" placeholder="First Name" name="firstname" required autofocus>
+
+                    <input id="lastname" type="text" placeholder="Last Name" name="lastname" required autofocus>
+
+                </div><br>
+                
+
+                <div display="inline-flex">
+                    <label style="margin-right: 2em;">Gender:  </label>
+
+                    <input id="male" type="radio" name="gender" value="Male" required><label for="male" style="margin-right: 2em;">Male</label>
+                    
+                    <input id="female" type="radio" name="gender" value="Female" required><label for="female">Female</label>
+
+                </div><br>
+
+                <input id="mobile" type="number" placeholder="Mobile Number" name="mobile" required><br>
+
+                <input id="email-sign" type="email" placeholder="Email" name="email-sign" required><br>
+
+                <input id="password-sign" type="password" placeholder="Password" name="password-sign" required><br>
+
+                <input id="password-confirm" type="password" placeholder="Re-type Password" name="password_confirmation" required><br><br>
+                <input type="submit" value="SIGN UP"><br>
+
+            </form>
+            </center>
+        
+        </div>
+
+
+    </div>
+
+
+    <script>
+        function openTabs(evt, tabName) {
+            // Declare all variables
+            var i, tabcontent, tablinks;
+
+            // Get all elements with class="tabcontent" and hide them
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+
+            // Get all elements with class="tablinks" and remove the class "active"
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+
+            // Show the current tab, and add an "active" class to the button that opened the tab
+            document.getElementById(tabName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+    </script>
+
+</div>
+
+<?php include VIEW_ROOT . 'layouts/footer.php'; ?>
